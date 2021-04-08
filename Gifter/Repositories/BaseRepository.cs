583e0bb682cd6b7979ff55661e52624b3376e5e8
sosinatolossa <sosinatolossa16@gmail.com>
2021-04-08 14:50:14 -0500

    initial commit

---
+++ Gifter/Repositories/BaseRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Gifter.Repositories
+{
+    //'abstract' It indicates that our BaseRepository class cannot be directly instantiated, 
+    //but can ONLY be used by inheritance
+    public abstract class BaseRepository
+    {
+        private readonly string _connectionString;
+
+        public BaseRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+       //We mark the Connection property as protected to make it available to child classes, 
+       //but inaccessible to any other code
+        protected SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_connectionString);
+            }
+        }
+    }
+}
