2fb350bdcecacf67c55e4975250da5000313d6c0
sosinatolossa <sosinatolossa16@gmail.com>
2021-04-12 14:18:48 -0500

    created CRUD operations for UserProfile

--- Gifter/Models/UserProfile.cs
+++ Gifter/Models/UserProfile.cs
@@ -14,6 +14,7 @@ namespace Gifter.Models
         public string Email { get; set; }
 
         public string ImageUrl { get; set; }
+        public string Bio { get; set; }
 
         [Required]
         public DateTime DateCreated { get; set; }
